a47bd8dc0df19d409d0d5376ac0e1f48db505825
donaldlu <donaldlu@tencent.com>
2025-05-23 14:01:46 +0800

    sample

--- libnativerender/src/main/cpp/render/sample_graphics.cpp
+++ libnativerender/src/main/cpp/render/sample_graphics.cpp
@@ -343,82 +343,107 @@ void SampleGraphics::DisPlayGPU() {
     DisPlay();
 }
 
+static void drawStringWithoutCustomFont(OH_Drawing_Canvas *canvas, const char *content, int x, int y) {
+    OH_Drawing_Brush *brush = OH_Drawing_BrushCreate();
+    OH_Drawing_BrushSetColor(brush, OH_Drawing_ColorSetArgb(0xFF, 0x00, 0x00, 0x00));
+    OH_Drawing_CanvasAttachBrush(canvas, brush);
+    OH_Drawing_Font *systemFont = OH_Drawing_FontCreate();
+    OH_Drawing_FontSetTextSize(systemFont, 17);
+    OH_Drawing_TextBlob *textBlob =
+        OH_Drawing_TextBlobCreateFromString(content, systemFont, OH_Drawing_TextEncoding::TEXT_ENCODING_UTF8);
+    OH_Drawing_CanvasDrawTextBlob(canvas, textBlob, x, y);
+    OH_Drawing_TextBlobDestroy(textBlob);
+    OH_Drawing_FontDestroy(systemFont);
+    OH_Drawing_CanvasDetachBrush(canvas);
+}
 
-// 字体引擎提供的文字绘制能力，可指定字体，自带排版，支持字体退化
-static void NativeOnDrawText(OH_Drawing_Canvas *canvas, const char *fontPath,const char *content, double fontHeight) {
+// 在类的实现部分添加这个静态方法
+static std::string extractFontFamily(const char *fontFilePath) {
+    std::string fontPath(fontFilePath);
+    size_t lastSlash = fontPath.find_last_of("/\\");
+    size_t lastDot = fontPath.find_last_of(".");
+    std::string fontName = "latex-";
+    if (lastSlash != std::string::npos && lastDot != std::string::npos && lastSlash < lastDot) {
+        fontName += fontPath.substr(lastSlash + 1, lastDot - lastSlash - 1);
+    }
+    return fontName;
+}
+
+static void NativeOnDrawText(OH_Drawing_Canvas *canvas,  const char *fontPath, const char *content, double fontHeight, int x,int y) {
+    std::string fontPathStr(fontPath);
+    if (fontPathStr.length() <= 0) {
+        drawStringWithoutCustomFont(canvas, content, x, y);
+        return;
+    }
+
+    OH_Drawing_FontCollection *fontCollection = OH_Drawing_CreateFontCollection();
+    const char *fontFamily = extractFontFamily(fontPath).c_str();
+    int errorCode = OH_Drawing_RegisterFont(fontCollection, fontFamily, fontPath);
+    const char *myFontFamilies[] = {fontFamily};
+
     // 选择从左到右/左对齐等排版属性
     OH_Drawing_TypographyStyle *typoStyle = OH_Drawing_CreateTypographyStyle();
     OH_Drawing_SetTypographyTextDirection(typoStyle, TEXT_DIRECTION_LTR);
-    OH_Drawing_SetTypographyTextAlign(typoStyle, TEXT_ALIGN_JUSTIFY);
+    OH_Drawing_SetTypographyTextAlign(typoStyle, TEXT_ALIGN_START);
+    OH_Drawing_SetTypographyTextFontFamily(typoStyle, fontFamily);
+    OH_Drawing_SetTypographyTextFontHeight(typoStyle, fontHeight);
 
-    // TEXT_ALIGN_JUSTIFY
-    // 设置文字颜色，例如黑色
     OH_Drawing_TextStyle *txtStyle = OH_Drawing_CreateTextStyle();
-    OH_Drawing_SetTextStyleColor(txtStyle, OH_Drawing_ColorSetArgb(0xFF, 0x00, 0x00, 0xFF));
-    // 设置文字大小、字重等属性
+    OH_Drawing_SetTextStyleColor(txtStyle, OH_Drawing_ColorSetArgb(0xFF, 0x00, 0x00, 0x00));
     OH_Drawing_SetTextStyleFontSize(txtStyle, 17);
     OH_Drawing_SetTextStyleFontWeight(txtStyle, FONT_WEIGHT_100);
-//    TEXT_BASELINE_ALPHABETIC
-//    TEXT_BASELINE_IDEOGRAPHIC
+    // 修改基线设置为ALPHABETIC，这样可以保持与TextBlob一致的绘制位置
     OH_Drawing_SetTextStyleBaseLine(txtStyle, TEXT_BASELINE_ALPHABETIC);
-    OH_Drawing_SetTextStyleFontHeight(txtStyle, fontHeight);
+    // 设置行高为1.0，避免额外的行间距影响
+//    OH_Drawing_SetTextStyleFontHeight(txtStyle, currentFont->getSize());
+    OH_Drawing_SetTextStyleFontHeight(txtStyle, fontHeight); // 0.01 影响 baseline？
+    // 设置字体类型等
+    OH_Drawing_SetTextStyleFontFamilies(txtStyle, 1, myFontFamilies);
+    OH_Drawing_SetTextStyleFontStyle(txtStyle, FONT_STYLE_NORMAL);
+    OH_Drawing_SetTextStyleLocale(txtStyle, "en");
+    OH_Drawing_TextStyleSetBaselineShift(txtStyle, 0.0);
 
+    OH_Drawing_TypographyCreate *handler = OH_Drawing_CreateTypographyHandler(typoStyle, fontCollection);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, txtStyle);
+    // 设置文字内容
+    OH_Drawing_TypographyHandlerAddText(handler, content);
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 10000);
+    OH_Drawing_TypographyPaint(typography, canvas, x, y);
+
+    // 销毁创建的资源
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextStyle(txtStyle);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+    OH_Drawing_DestroyFontCollection(fontCollection);
+}
+
+static void NativeOnDrawLine(OH_Drawing_Canvas *canvas, int x, int y) {
+    // 绘制y所在的线
     OH_Drawing_Pen *pen = OH_Drawing_PenCreate();
     OH_Drawing_PenSetColor(pen, OH_Drawing_ColorSetArgb(0xFF, 0xFF, 0x00, 0x00));
     OH_Drawing_PenSetWidth(pen, 0.3f);
     OH_Drawing_CanvasAttachPen(canvas, pen);
 
-    // 设置字体类型等
-    OH_Drawing_Matrix *matrix = OH_Drawing_MatrixCreateTranslation(0, adaptationUtil->GetHeight(30.f));
-    // 对Canvas进行矩阵变换
-    OH_Drawing_CanvasConcatMatrix(canvas, matrix);
-    OH_Drawing_MatrixDestroy(matrix);
+    OH_Drawing_CanvasDrawLine(canvas, x, y, x + 200, y);
 
-    OH_Drawing_FontCollection *fontCollection = OH_Drawing_CreateFontCollection();
-    const char *fontFamily = "myFamilyName";
-    OH_Drawing_RegisterFont(fontCollection, fontFamily, fontPath);
-    const char *myFontFamilies[] = {"myFamilyName"};
-    OH_Drawing_SetTextStyleFontFamilies(txtStyle, 1, myFontFamilies);
-    OH_Drawing_SetTextStyleFontStyle(txtStyle, FONT_STYLE_NORMAL);
-    OH_Drawing_SetTextStyleLocale(txtStyle, "en");
-    OH_Drawing_TypographyCreate *handler = OH_Drawing_CreateTypographyHandler(typoStyle, fontCollection);
-    OH_Drawing_TypographyHandlerPushTextStyle(handler, txtStyle);
-    // 设置文字内容
-    OH_Drawing_TypographyHandlerAddText(handler, content);
-    OH_Drawing_TypographyHandlerPopTextStyle(handler);
-    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
-    // 设置页面最大宽度
-    double maxWidth = 1000;
-    OH_Drawing_TypographyLayout(typography, maxWidth);
-    // 设置文本在画布上绘制的起始位置
-    double x = 10;
-    double y = 10;
-
-
-    // 将文本绘制到画布上
-    OH_Drawing_CanvasDrawLine(canvas, x, y, x + 100, y);
-
-    OH_Drawing_TypographyPaint(typography, canvas, x, y);
-
-
-    // 销毁创建的资源
-    OH_Drawing_DestroyTypography(typography);
-    OH_Drawing_DestroyTypographyHandler(handler);
-    OH_Drawing_DestroyFontCollection(fontCollection);
-
-    OH_Drawing_DestroyTypographyStyle(typoStyle);
-    OH_Drawing_DestroyTextStyle(txtStyle);
     OH_Drawing_PenDestroy(pen);
+    OH_Drawing_CanvasDetachPen(canvas);
 }
 
-void SampleGraphics::DrawMyTest(OH_Drawing_Canvas *canvas, const char *fontPath, const char *content, double fontHeight) {
+void SampleGraphics::DrawMyTest(OH_Drawing_Canvas *canvas, const char *fontPath, const char *content,
+                                double fontHeight) {
     SAMPLE_LOGI("DrawMyTest");
-    // 先放大十倍
+    // 先放大十倍(模拟实际场景需要)
     OH_Drawing_Matrix *matrix = OH_Drawing_MatrixCreateScale(10, 10, 0, 0);
     OH_Drawing_CanvasConcatMatrix(canvas, matrix);
     OH_Drawing_MatrixDestroy(matrix);
 
-    NativeOnDrawText(canvas, fontPath, content, fontHeight);
+    int x = 10;
+    int y = 10;
+    NativeOnDrawText(canvas, fontPath, content, fontHeight, x, y);
+    NativeOnDrawLine(canvas, x, y);
 }
 
 void SampleGraphics::DoRender(SampleGraphics *render, char *canvasType, char *shapeType) {
